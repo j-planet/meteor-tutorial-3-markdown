be08b67fcdb8d77737b1730431b280aa8d1ed749
jennyyuejin <jin.yuejenny@gmail.com>
2016-08-08 17:48:42 -0700

    added publish to server

--- server/main.jsx
+++ server/main.jsx
@@ -1,7 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Bins } from '../imports/collections/bins';
 
+
 Meteor.startup(() =>
 {
     // code to run on the server at startup
+    // can NOT use a fat arrow, which wouldn't let us do this.userid
+    Meteor.publish('bins', function() {
+        return Bins.find({ ownerId: this.userId });
+    });
 });
